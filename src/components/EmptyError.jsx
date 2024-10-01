6de1356932c2b27be088331936f21ceb0326861a
Milad salamian <miladsalami1385@gmail.com>
2024-10-01 14:31:46 -0700

    deploy on vercel

--- src/components/EmptyError.jsx
+++ src/components/EmptyError.jsx
@@ -4,7 +4,7 @@ import { FaMagnifyingGlass } from "react-icons/fa6";
 function EmptyError({ msg }) {
   return (
     <div className="bg-red-600 text-sm md:text-2xl rounded-md text-white flex items-center justify-center gap-x-3 py-10">
-      <span>{msg}</span>
+      <span className="text-sm md:text-2xl">{msg}</span>
       <FaMagnifyingGlass />
     </div>
   );
